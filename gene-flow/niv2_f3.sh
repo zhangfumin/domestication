a4ad74cb7736fc53fcc47d21734ef1723e233909
zhangfumin <zhangfm@ibcas.ac.cn>
2022-06-30 15:23:46 +0800

    a2

---
+++ gene-flow/niv2_f3.sh
@@ -0,0 +1,2 @@
+#Two files are required to run qp3pop, the  parfile and logfile. The parfile includes four parameters: input genotype file, input snp file, input indiv file, and list for qp3test. The logfile contains the output of the run. 
+/AdmixTools/bin/qp3Pop -p niv2_f3_parfile > niv2_f3_logfile
---
+++ gene-flow/niv2_qpDstat.sh
@@ -0,0 +1,2 @@
+#Two files are required to run qpDstat, the  parfile and logfile. The parfile includes four parameters: input genotype file, input snp file, input indiv file, and list for qpDstat. The logfile contains the output of the run.
+/AdmixTools/bin/qpDstat -p  niv2_qpDstat_parfile > niv2_qpDstat_logfile
